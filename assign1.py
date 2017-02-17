14efa6f33eb55d5ba5f03bcb5a8c7b456f4e0b93
Tuan Dinh <tuandinh91@gmail.com>
2017-02-17 12:35:16 -0600

    modified inputs

--- assign1.py
+++ assign1.py
@@ -8,6 +8,10 @@ matplotlib.use('Agg')
 
 from matplotlib import pyplot as plt
 
+#global arrays
+OYPTRANS = []
+OYHPL = []
+
 def draw_chart(OY,file_name_modifier=""):
 
     #x-axis data
@@ -75,27 +79,34 @@ def calculate_ptrans():
         for item in content:
             if item.startswith('WR11C2R4'):
                 return float(item.split()[-1])
+                
+def process_cluster(cluster, nb, n):
+    global OYPTRANS
+    global OYHPL
+    delete_output_file()
+    change_input_parameter(1,cluster,nb,n)
+
+    os.system("mpirun -np "+str(cluster*2)+" --hostfile ~/mpi_host"+str(cluster)+" -"+args.sched+" ./hpcc")
+    
+    print "\nCluster "+str(cluster)+": "
+    file = open('hpccoutf.txt','r')
+    PTRANS_GBs =  file.read().split('PTRANS_GBs=',1)[1].split('\nPTRANS_time',1)[0]
+    HPL_Gflops=calculate_ptrans()
+    print 'PTRANS_GBs: '+str(PTRANS_GBs)
+    print 'HPL_Gflops: '+str(HPL_Gflops)
+    OYPTRANS.append(float(PTRANS_GBs))  
+    OYHPL.append(float(HPL_Gflops))                
                     
 def hpcc():
+    global OYPTRANS
+    global OYHPL
     print 'hpcc'
     print args.sched
     os.chdir("hpcc-1.4.3/")
-    OYPTRANS = []
-    OYHPL = []
-    for cluster in range(1,5):    
-        delete_output_file()
-        change_input_parameter(1,cluster,50,200)
-    
-        os.system("mpirun -np "+str(cluster*2)+" --hostfile ~/mpi_host"+str(cluster)+" -"+args.sched+" ./hpcc")
-        
-        print "\nCluster "+str(cluster)+": "
-        file = open('hpccoutf.txt','r')
-        PTRANS_GBs =  file.read().split('PTRANS_GBs=',1)[1].split('\nPTRANS_time',1)[0]
-        HPL_Gflops=calculate_ptrans()
-        print 'PTRANS_GBs: '+str(PTRANS_GBs)
-        print 'HPL_Gflops: '+str(HPL_Gflops)
-        OYPTRANS.append(float(PTRANS_GBs))  
-        OYHPL.append(float(HPL_Gflops))   
+    process_cluster(1, 224, 4853); 
+    process_cluster(2, 224, 6944); 
+    process_cluster(3, 224, 8512); 
+    process_cluster(4, 224, 9760); 
     draw_chart(OYPTRANS,"PTRANS")
     draw_chart(OYPTRANS,"HPL")
 
