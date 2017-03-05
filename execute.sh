8bcf2353781b1418b5ee3f858593d76b044787a5
Tuan Dinh <tuandinh91@gmail.com>
2017-03-05 01:58:38 -0600

    Add files via upload

--- execute.sh
+++ execute.sh
@@ -8,4 +8,5 @@ bin/hadoop jar contrib/streaming/hadoop-streaming-1.2.1.jar \
 -file /home/hduser/mapper.py -mapper /home/hduser/mapper.py \
 -file /home/hduser/reducer.py -reducer /home/hduser/reducer.py
 #copy output to local
-bin/hadoop fs -copyToLocal -f /hw2-output ~/out
+rm -rf ~/out-1
+bin/hadoop fs -copyToLocal /hw2-output ~/out-1
