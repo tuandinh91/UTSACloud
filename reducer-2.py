141bb7d370e6b9658a8ee64ff0e5572155a36de1
Tuan Dinh <tuandinh91@gmail.com>
2017-03-05 00:19:32 -0600

    Add files via upload
    fixed mapper-2 and reducer-2

--- reducer-2.py
+++ reducer-2.py
@@ -17,15 +17,14 @@ def printline(crime, month_list):
     print crime    
     
 current_crime = None
-current_month_list = newMonthArray()
-
-
+current_month_list = None
 crime = None
 
 for line in sys.stdin:
     line = line.strip()
     #crime, month, count = line.split('\t',2)
     items = line.split('\t',2)
+    
     #invalid of input length
     if (len(items) != 3):
         continue
@@ -42,16 +41,21 @@ for line in sys.stdin:
             continue
     except ValueError:
         continue
-        
-    #check if the same crime    
+
+    #check if the same crime   
     if current_crime == crime:
         #increase the count in the matrix
         current_month_list[month-1] += count
+        
     else:
-        #different crime, then print out the matrix
-        printline(crime, current_month_list)
+        if current_month_list is not None:
+            #different crime, then print out the matrix
+            printline(current_crime, current_month_list)
             
         #initialize new crime
         current_crime = crime
         current_month_list = newMonthArray()
         current_month_list[month-1] += count
+
+#print the last crime
+printline(crime, current_month_list)
