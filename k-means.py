51bb1dafd7454c3bc01eeb93e3b7519797713594
Sharif Mohammad Shahnewaz Ferdous <sharif.shahnewaz@gmail.com>
2017-03-27 18:35:30 -0500

    added comment

--- k-means.py
+++ k-means.py
@@ -5,7 +5,7 @@ import sys
 import numpy as np
 from pyspark import SparkContext
 
-
+#this function reports the GPS of thefts and burglaries
 def parseVector(line):
     parts = line.split(',')
     if len(parts[6])>2 and len(parts[7])>2 and parts[6]!="LONGITUDE":
@@ -13,7 +13,7 @@ def parseVector(line):
             return "DATA",(float(parts[6]),float(parts[7]))
     return "NODATA",[0,0]
 
-
+#finds the closest centroid
 def closestPoint(p, centers):
     bestIndex = 0
     closest = float("+inf")
@@ -31,31 +31,37 @@ if __name__ == "__main__":
         print("Usage: kmeans <file> <k> <convergeDist>", file=sys.stderr)
         exit(-1)
 
-
     sc = SparkContext(appName="PythonKMeans")
     lines = sc.textFile((sys.argv[1]),3)
-    data = lines.map(parseVector).groupByKey().mapValues(list).cache()
-    data = data.filter(lambda x : "NODATA" not in x[0]).flatMap(lambda s: s[1]).cache()
-
+    #filter out data not related to theft or burglaries
+    data = lines.map(parseVector).filter(lambda x : "NODATA" not in x[0]).map(lambda s: s[1]).cache()
+    
     K = int(sys.argv[2])
     
     convergeDist = float(sys.argv[3])
-   
+
+    #initialization k points 
     kPoints = data.takeSample(False, K, 1)
     tempDist = 1.0
 
     while tempDist > convergeDist:
+        #assigning to nearest centroid and keeping count
         closest = data.map(
             lambda p: (closestPoint(p, kPoints), (p, 1)))
+            
+        #adding all values under same cluster
         pointStats = closest.reduceByKey(
             lambda p1_c1, p2_c2: ((p1_c1[0][0] + p2_c2[0][0],p1_c1[0][1] + p2_c2[0][1]), p1_c1[1] + p2_c2[1]))
-       
+            
+        #calculating new positions
         newPoints = pointStats.map(
             lambda st: (st[0], (st[1][0][0] / st[1][1], st[1][0][1] / st[1][1]))).collect()
-        #for x in newPoints:
-        #    print (x)
+          
+        #new distance to test for convergence          
         tempDist = sum(((kPoints[iK][0] - p[0]) ** 2+(kPoints[iK][1] - p[1]) ** 2) for (iK, p) in newPoints)
         print ("tempdist",tempDist)
+        
+        #assigning new points
         for (iK, p) in newPoints:
             kPoints[iK] = p
 
